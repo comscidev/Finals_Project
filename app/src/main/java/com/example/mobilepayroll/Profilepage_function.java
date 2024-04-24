5282ed88b29cb8560a7451b81153db9125ce6fa7
comscidev <csdeveloper31@gmail.com>
2024-04-24 14:51:39 +0800

    With profile pic

--- app/src/main/java/com/example/mobilepayroll/Profilepage_function.java
+++ app/src/main/java/com/example/mobilepayroll/Profilepage_function.java
@@ -1,12 +1,16 @@
 package com.example.mobilepayroll;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,23 +30,44 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.google.rpc.context.AttributeContext;
+import com.squareup.picasso.Picasso;
 
 public class Profilepage_function extends AppCompatActivity {
+    private ImageView profile_pic;
+    StorageReference storageReference;
+    FirebaseAuth Auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profilepage_function);
+        FirebaseAuth Auth = FirebaseAuth.getInstance();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+        String userID = Auth.getCurrentUser().getUid();
+
         TextView fname = findViewById(R.id.textFullName);
         TextView username = findViewById(R.id.textusername);
         TextView email = findViewById(R.id.showemail);
         TextView change_pass = findViewById(R.id.showpass);
         ImageView back = findViewById(R.id.backIcon);
         ImageButton change_email = findViewById(R.id.profile_editIcon);
-        FirebaseAuth Auth = FirebaseAuth.getInstance();
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        String userID = Auth.getCurrentUser().getUid();
+         profile_pic = findViewById(R.id.userprofile);
+        ImageButton insert_pic = findViewById(R.id.insert_pic);
+        StorageReference profileref = storageReference.child("users/"+Auth.getCurrentUser().getUid()+ "/profile.jpg");
+
+        profileref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(profile_pic);
+            }
+        });
+
 
 
         DocumentReference documentReference = db.collection("users").document(userID);
@@ -189,10 +214,63 @@ public class Profilepage_function extends AppCompatActivity {
             }
         });
 
+        insert_pic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGalleryIntent, 1000);
+            }
+        });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000) {
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                Uri imageuri = data.getData();
+                if (imageuri != null) {
+                    uploadImage(imageuri);
+                    StorageReference fileref = storageReference.child("users/" + Auth.getCurrentUser().getUid() + "/profile.jpg");
+                    fileref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            Picasso.get().load(uri).into(profile_pic);
+                        }
+                    });
+                }
+            }
+        }
+    }
+
+
+    private void uploadImage(Uri imageuri) {
+        FirebaseAuth Auth = FirebaseAuth.getInstance(); // Initialize FirebaseAuth here
+        StorageReference fileref = storageReference.child("users/" + Auth.getCurrentUser().getUid() + "/profile.jpg");
+
+        fileref.putFile(imageuri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                fileref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Picasso.get().load(uri).into(profile_pic);
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Profilepage_function.this, "Failed to upload", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
     public boolean isValidPassword(String password) {
         return password.length() >= 6 && password.matches
                 ("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]+$");
     }
+
 }
