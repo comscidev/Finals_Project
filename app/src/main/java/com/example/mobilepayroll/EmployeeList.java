8785a0ff302ae7c3000abf5b760c4eabb62dbfd5
comscidev <csdeveloper31@gmail.com>
2024-04-25 20:48:48 +0800

    update

--- app/src/main/java/com/example/mobilepayroll/EmployeeList.java
+++ app/src/main/java/com/example/mobilepayroll/EmployeeList.java
@@ -1,5 +1,7 @@
 package com.example.mobilepayroll;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,18 +10,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class EmployeeList extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_employee_list);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_employees);
+
+//        bottomNavigationView.setOnItemSelectedListener(item -> {
+//            switch (item.getItemId()){
+//                case R.id.bottom_employees:
+//                    return true;
+//                case R.id.bottom_profile:
+//                    startActivity(new Intent(getApplicationContext(), Profilepage_function.class));
+//                    finish();
+//                    return true;
+//            }
+//            return false;
+//        });
     }
 }
